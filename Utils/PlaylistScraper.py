33a682ec91fb0dc23a3e585693057633dd0d0679
최재용 <johnchoi213.jc@gmail.com>
2023-06-15 14:21:17 +0900

    Update PlaylistScraper.py

--- Utils/PlaylistScraper.py
+++ Utils/PlaylistScraper.py
@@ -1,7 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import json
-import re
+import json, re, sys, os
+import confluent_kafka
+from datetime import datetime
 
 SCOPE = ['user-library-read',
          'user-follow-read',
@@ -10,10 +11,10 @@ SCOPE = ['user-library-read',
          'playlist-read-collaborative',
          'playlist-modify-public',
          'playlist-modify-private']
-#USER_ID = ''
-REDIRECT_URI = ''
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+#USER_ID = '	31itojgy5yhdtj4u6ej6zhwgi7oe'
+REDIRECT_URI = 'http://localhost:3000'
+CLIENT_ID = '48a6aeac07b3427698edf6628cf93548'
+CLIENT_SECRET = '7d258986e8a0464b93ab011768d1282f'
 auth_manager = SpotifyOAuth(
     scope=SCOPE,
     #username=USER_ID,
@@ -21,7 +22,96 @@ auth_manager = SpotifyOAuth(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET)
 
-def get_categories():
+schema = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+    "playlists": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "name": {
+            "type": "string"
+          },
+          "collaborative": {
+            "type": "string"
+          },
+          "pid": {
+            "type": "integer"
+          },
+          "modified_at": {
+            "type": "integer"
+          },
+          "num_tracks": {
+            "type": "integer"
+          },
+          "num_albums": {
+            "type": "integer"
+          },
+          "num_followers": {
+            "type": "integer"
+          },
+          "tracks": {
+            "type": "array",
+            "items": {
+              "type": "object",
+              "properties": {
+                "pos": {
+                  "type": "integer"
+                },
+                "artist_name": {
+                  "type": "string"
+                },
+                "track_uri": {
+                  "type": "string"
+                },
+                "artist_uri": {
+                  "type": "string"
+                },
+                "track_name": {
+                  "type": "string"
+                },
+                "album_uri": {
+                  "type": "string"
+                },
+                "duration_ms": {
+                  "type": "integer"
+                },
+                "album_name": {
+                  "type": "string"
+                }
+              },
+              "required": [
+                "pos",
+                "artist_name",
+                "track_uri",
+                "artist_uri",
+                "track_name",
+                "album_uri",
+                "duration_ms",
+                "album_name"
+              ]
+            }
+          }
+        },
+        "required": [
+          "name",
+          "collaborative",
+          "pid",
+          "modified_at",
+          "num_tracks",
+          "num_albums",
+          "num_followers",
+          "tracks"
+        ]
+      }
+    }
+  },
+  "required": ["playlists"]
+}
+
+def get_categories(search_category):
     try:
         sp = spotipy.Spotify(auth_manager=auth_manager)
         query_limit = 50
@@ -29,7 +119,7 @@ def get_categories():
         new_offset = 0
 
         while True:
-            results = sp.category_playlists(category_id='hiphop', limit=query_limit, country='US', offset=new_offset)
+            results = sp.category_playlists(category_id=search_category, limit=query_limit, country='US', offset=new_offset)
             #results는 플레이리스트 정보만 읽어온다.
 
             for item in results['playlists']['items']:
@@ -47,39 +137,100 @@ def get_categories():
                     })
             new_offset = new_offset + query_limit
             next = results['playlists']['next']
+
             if next is None:
                 break
+
         return categories
     except Exception as e:
         print('Failed to upload to call get_categories: ' + str(e))
 
+def get_playlists_with_keyword(keyword, limit):
+    playlists = []
+    offset = 0
+    total = 0
+
+    while total < limit:
+        results = sp.search(q=keyword, type='playlist', limit=50, offset=offset)
+        items = results['playlists']['items']
+        playlists.extend(items)
+        total += len(items)
+        offset += len(items)
+
+        if len(items) < 50:
+            break
+
+    return playlists[:limit]
+
 def get_songs(categories):
     try:
         sp = spotipy.Spotify(auth_manager=auth_manager)
-        songs = []
+        playlists = []
+        playlist_id_set = set()
 
         for category in categories:
             if category is None:
                 break
             playlist_id = category['playlist_id']
             results = sp.playlist(playlist_id=playlist_id)
-            print(results)
-            exit(0)
+            pos = 0
+            tracks = []
+            album_ids = set()
+            modified_at = '1970-01-01T00:00:00Z'
+
+            if results['id'] in playlist_id_set:
+                continue
+
             #플리에 따른 트랙을 읽어오는 부분
             for item in results['tracks']['items']:
                 if (item is not None and item['track'] is not None and item['track']['id'] is not None and
                         item['track']['name'] is not None and item['track']['external_urls']['spotify'] is not None):
-                    songs.append({
-                        'id': item['track']['id'],
-                        'name': item['track']['name'],
-                        'url': item['track']['external_urls']['spotify']
+                    album_ids.add(item['track']['album']['id'])
+
+                    if modified_at < item['added_at']:
+                        modified_at = item['added_at']
+
+                    tracks.append({
+                        'pos': pos,
+                        'artist_name': item['track']['album']['artists'][0]['name'],
+                        'track_uri': item['track']['uri'],
+                        'artist_uri': item['track']['album']['artists'][0]['uri'],
+                        'track_name': item['track']['name'],
+                        'album_uri': item['track']['album']['uri'],
+                        'duration_ms': item['track']['duration_ms'],
+                        'album_name': item['track']['album']['name']
                     })
+                    pos += 1
                 else:
                     break
-        return songs
+
+            playlists.append({
+                'name': results['name'],
+                'collaborative': results['collaborative'],
+                'pid': results['id'],
+                'modified_at': int(datetime.strptime(modified_at, '%Y-%m-%dT%H:%M:%SZ').timestamp()),
+                'num_tracks': results['tracks']['total'],
+                'num_albums': len(album_ids),
+                'num_followers': results['followers']['total'],
+                'tracks': tracks
+            })
+
+        return playlists
     except Exception as e:
         print('Failed to upload to call get_songs: ' + str(e))
 
-categories = get_categories()
-songs = get_songs(categories)
-print(json.dumps(songs))
+print("Running main.py")
+
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+search_category = sys.argv[1]
+categories = get_categories(search_category)
+playlists = get_songs(categories)
+
+output_file_name = sys.argv[1]
+output_file_path = os.path.join('D:', output_file_name + '.json')
+
+with open(output_file_path, 'w') as file:
+    json.dump({'playlists': playlists}, file)
+
+#validate(instance = json.loads(playlists), schema = schema)
