db44f8dc92f3c15e7f3141413317a180bfa2bf67
최재용 <johnchoi213.jc@gmail.com>
2023-06-15 10:52:50 +0900

    Update script.sh

--- Utils/script.sh
+++ Utils/script.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-python PlaylistScraper.py hiphop
+#Category arguments
+#args=("hiphop" "rock" "jazz" "pop" "classic" "blues" "country" "disco")
+args=("hiphop" "rock" "jazz")
+
+# Iterate over the arguments and run the Python script with each argument
+for arg in "${args[@]}"; do
+  python D:/pythonProject/main.py "$arg" &
+done
+
+wait
+
 read -p "Press any key to exit..."
